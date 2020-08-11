c6eb29ee740e0340e6383dabca88f33af016116a
YogPanjarale <64301340+YogPanjarale@users.noreply.github.com>
2020-08-11 17:11:12 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,6 +1,11 @@
 class SpiderMan extends BaseClass {
   constructor(x,y){
+  if(height/20 >40){
     super(x,y,height/20,height/20);
+  }else if(height/20 <40){
+    super(x,y,50,50);
+  }
+    
     this.image = loadImage("SpiderMan.png");
    // this.smokeImage = loadImage("sprites/smoke.png");
   
