7e26477a3535eb8e97065f1ae4097e5fd7e53d76
YogPanjarale <64301340+YogPanjarale@users.noreply.github.com>
2020-08-11 17:12:18 +0530

    1.4

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var engine, world;
 var player,web
 var Score=0;
 function setup() {
-  console.log("1.3")
+  console.log("1.4")
   createCanvas(windowWidth,windowHeight);
   //createSprite(400, 200, 50, 50);
   //scale(1000/height)
